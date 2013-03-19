27b266c504598f3978a2fc581d855228291b1ca0
Wil Schultz <wschultz@bsdboy.com>
2013-03-19 16:42:14 -0700

    here we go

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,8 @@ license          ""
 description      "Installs/Configures jrebel-license-server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
+
+%w{ java }.each do |cb|
+  depends cb
+end
+
